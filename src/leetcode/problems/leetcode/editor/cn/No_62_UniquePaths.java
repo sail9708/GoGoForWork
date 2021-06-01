e170c544b2bccdf0887ec63290a6cb90af733ffa
Northtobridge <876211428@qq.com>
2021-06-01 16:09:17 +0800

    add

---
+++ src/leetcode/problems/leetcode/editor/cn/No_62_UniquePaths.java
@@ -0,0 +1,77 @@
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。 
+//
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。 
+//
+// 问总共有多少条不同的路径？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：m = 3, n = 7
+//输出：28 
+//
+// 示例 2： 
+//
+// 
+//输入：m = 3, n = 2
+//输出：3
+//解释：
+//从左上角开始，总共有 3 条路径可以到达右下角。
+//1. 向右 -> 向下 -> 向下
+//2. 向下 -> 向下 -> 向右
+//3. 向下 -> 向右 -> 向下
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：m = 7, n = 3
+//输出：28
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：m = 3, n = 3
+//输出：6 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= m, n <= 100 
+// 题目数据保证答案小于等于 2 * 109 
+// 
+// Related Topics 数组 动态规划 
+// 👍 1005 👎 0
+
+  
+package leetcode.problems.leetcode.editor.cn;
+public class No_62_UniquePaths{
+    public static void main(String[] args) {
+       Solution solution = new No_62_UniquePaths().new Solution();
+       solution.uniquePaths(4,4);
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int uniquePaths(int m, int n) {
+        long numerator = 1; // 分子
+        int denominator = m - 1; // 分母
+        int count = m - 1;
+        int t = m + n - 2;
+        for (; count>0;count--) {
+            numerator *= (t--);
+            while (denominator != 0 && numerator % denominator == 0) {
+                numerator /= denominator;
+                denominator--;
+            }
+        }
+        return (int)numerator;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
