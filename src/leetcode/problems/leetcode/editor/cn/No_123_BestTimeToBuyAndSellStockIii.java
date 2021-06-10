78f441ffb4c635d4e2f10b37535ba77532585e57
Northtobridge <876211428@qq.com>
2021-06-10 17:44:17 +0800

    modify

--- src/leetcode/problems/leetcode/editor/cn/No_123_BestTimeToBuyAndSellStockIii.java
+++ src/leetcode/problems/leetcode/editor/cn/No_123_BestTimeToBuyAndSellStockIii.java
@@ -67,9 +67,9 @@ class Solution {
      *
      */
     public int maxProfit(int[] prices) {
+        int buy2 = -prices[0], sell2 = 0;
         int n = prices.length;
         int buy1 = -prices[0], sell1 = 0;
-        int buy2 = -prices[0], sell2 = 0;
         for (int i = 1; i < n; ++i) {
             buy1 = Math.max(buy1, -prices[i]);
             sell1 = Math.max(sell1, buy1 + prices[i]);
