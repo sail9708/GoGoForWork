ea9dafc2dca9b49763c8fc981aea7ee4f9cb0e27
Northtobridge <876211428@qq.com>
2021-10-12 23:20:31 +0800

    add

---
+++ src/leetcode/problems/leetcode/editor/cn/No_29_DivideTwoIntegers.java
@@ -0,0 +1,96 @@
+//给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。 
+//
+// 返回被除数 dividend 除以除数 divisor 得到的商。 
+//
+// 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: dividend = 10, divisor = 3
+//输出: 3
+//解释: 10/3 = truncate(3.33333..) = truncate(3) = 3 
+//
+// 示例 2: 
+//
+// 输入: dividend = 7, divisor = -3
+//输出: -2
+//解释: 7/-3 = truncate(-2.33333..) = -2 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 被除数和除数均为 32 位有符号整数。 
+// 除数不为 0。 
+// 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2³¹, 231 − 1]。本题中，如果除法结果溢出，则返回 231 − 1。 
+// 
+// Related Topics 位运算 数学 👍 736 👎 0
+
+  
+package leetcode.problems.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No_29_DivideTwoIntegers{
+    public static void main(String[] args) {
+       Solution solution = new No_29_DivideTwoIntegers().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int divide(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            }
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
+
+        // 一般情况，使用类二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        boolean rev = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            rev = !rev;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            rev = !rev;
+        }
+
+        List<Integer> candidates = new ArrayList<Integer>();
+        candidates.add(divisor);
+        int index = 0;
+        // 注意溢出
+        while (candidates.get(index) >= dividend - candidates.get(index)) {
+            candidates.add(candidates.get(index) + candidates.get(index));
+            ++index;
+        }
+        int ans = 0;
+        for (int i = candidates.size() - 1; i >= 0; --i) {
+            if (candidates.get(i) >= dividend) {
+                ans += 1 << i;
+                dividend -= candidates.get(i);
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
